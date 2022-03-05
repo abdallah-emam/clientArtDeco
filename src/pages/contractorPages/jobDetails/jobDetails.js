7b9e8c11cd8c9741393c161295ec80740946136f
DoaaEldesoki <doaa.eldesoki6@gmail.com>
2022-03-05 23:29:40 +0200

    adding style to jobdetails and profile

--- src/pages/contractorPages/jobDetails/jobDetails.js
+++ src/pages/contractorPages/jobDetails/jobDetails.js
@@ -3,21 +3,45 @@ import React from "react";
 import "./jobDetails.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Link } from "react-router-dom";
+import BTN from "../../../components/button/btn";
 
 export default function JobDetails() {
   return (
     <div>
-      <br></br>
-      <br></br>
-      <div className="container">
-        <h1> Job Details </h1>
-        <div className="d-style btn btn-brc-tp border-2 bgc-white btn-outline-purple btn-h-outline-purple btn-a-outline-purple w-100 my-2 py-3 shadow-sm">
+     
+      <div className="container my-5">
+        <h2 className="mx-2"> Job Details </h2>
+       
+        <div className=" border-2 bgc-white btn-outline-purple btn-h-outline-purple btn-a-outline-purple w-100 my-2 p-3 shadow-sm">
+        <div className="row my-4">
+            <div className='col-12 col-md-6'>
+              <div className="row"> 
+              <div className="col-4">
+                <h5> Client</h5>
+                <p>Doaa. A</p>
+              </div>
+              <div className="col-4">
+                <h5> Duration</h5>
+                <p> 20</p>
+              </div>
+              <div className="col-4">
+                <h5> Budget</h5>
+                <p>50</p>
+              </div>
+              
+
+
+              </div>
+            </div>
+
+
+
+          </div>
           <div className="row align-items-center">
             <ul className="list-unstyled mb-0 col-12 col-md-8 text-dark-l1 text-90 text-left my-4 my-md-0">
               <li>
-                <i className="fa fa-check text-success-m2 text-110 mr-2 mt-1"></i>
+                <i className=" text-success-m2 text-110 mr-2 mt-1"></i>
                 <span>
-                  &nbsp;{" "}
                   <span className="text-110">
                     Lorem Ipsum is simply dummy text of the printing and
                     typesetting industry. Lorem Ipsum has been the industry's
@@ -27,137 +51,17 @@ export default function JobDetails() {
                   </span>
                 </span>
               </li>
-              <li className="mt-25">
-                <i className="fa fa-check text-success-m2 text-110 mr-2 mt-1"></i>
-                &nbsp;<span className="text-110">Molestie nunc non</span>
-              </li>
             </ul>
-            <div className="col-12 col-md-4 text-center ">
-              <Link
-                to={"/JobProposal/622216d2cac0a0058f6d8dfa"}
-                className="f-n-hover btn btn-back-color btn-raised px-4 py-25 w-75 text-600 m-3"
-              >
-                <span>Send a Proposal</span>
-              </Link>
-
-
+            <div className="col-12 col-md-4 text-center my-3 my-md-0 ">
+              <BTN URL="/JobProposal/622216d2cac0a0058f6d8dfa" text="Send a Proposal" type="defult" />
             </div>
             <div className="row align-items-center">
-              <ul className="list-unstyled mb-0 col-12 col-md-8 text-dark-l1 text-90 text-left my-4 my-md-0">
-                <li>
-                  <i className="fa fa-check text-success-m2 text-110 mr-2 mt-1"></i>
-                  <span>
-                    &nbsp;{" "}
-                    <span className="text-110">
-                      Lorem Ipsum is simply dummy text of the printing and
-                      typesetting industry. Lorem Ipsum has been the industry's
-                      standard dummy text ever since the 1500s, when an unknown
-                      printer took a galley of type and scrambled it to make a
-                      type specimen book. It has survived not only five
-                      centuries
-                    </span>
-                  </span>
-                </li>
-              </ul>
-              <div className="container p-4">
-                <div className="row">
-                  <div className="col">
-                    <div className="card" style={{ width: "18rem" }}>
-                      <img src={""} className="card-img-top" alt="..." />
-                      <div className="card-body">
-                        <p className="card-text">
-                          Some quick example text to build on the card title and
-                          make up the bulk of the card's content.
-                        </p>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="col">
-                    <div className="card" style={{ width: "18rem" }}>
-                      <img src={''} className="card-img-top" alt="..." />
-                      <div className="card-body">
-                        <p className="card-text">
-                          Some quick example text to build on the card title and
-                          make up the bulk of the card's content.
-                        </p>
-                      </div>
-                    </div>{" "}
-                  </div>
-                  <div className="col">
-                    <div className="card" style={{ width: "18rem" }}>
-                      <img src={''} className="card-img-top" alt="..." />
-                      <div className="card-body">
-                        <p className="card-text">
-                          Some quick example text to build on the card title and
-                          make up the bulk of the card's content.
-                        </p>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="d-style btn btn-brc-tp border-2 bgc-white btn-outline-purple btn-h-outline-purple btn-a-outline-purple w-100 my-2 py-3 shadow-sm">
-                <div className="row align-items-center">
-                  <ul className="list-unstyled mb-0 col-12 col-md-4 text-dark-l1 text-90 text-left my-4 my-md-0">
-                    <li>
-                      <i className="fa fa-check text-success-m2 text-110 mr-2 mt-1"></i>
-                      <span>
-                        <span className="text-110">
-                          {" "}
-                          <h4 className="pt-3 text-170 text-600 btn-back-color-d1 letter-spacing ">
-                            skills and Expertise{" "}
-                          </h4>
-                        </span>
-                        <span className="badge rounded-pill btn-back-color text-dark m-2 text-white">
-                          {" "}
-                          lorem
-                        </span>{" "}
-                        <span className="badge rounded-pill btn-back-color text-dark m-2 text-white">
-                          Warning
-                        </span>
-                        <span className="badge rounded-pill btn-back-color text-dark m-2 text-white">
-                          lorem
-                        </span>
-                        <span className="badge rounded-pill btn-back-color text-dark m-2 text-white">
-                          {" "}
-                          lorem
-                        </span>{" "}
-                        <span className="badge rounded-pill btn-back-color text-dark m-2 text-white">
-                          Warning
-                        </span>
-                        <span className="badge rounded-pill btn-back-color text-dark m-2 text-white">
-                          lorem
-                        </span>
-                      </span>
-                    </li>
-
-                    <li className="mt-25">
-                      <i className="fa fa-check text-success-m2 text-110 mr-2 mt-1"></i>
-                      &nbsp;<span className="text-110">Molestie nunc non</span>
-                    </li>
-                  </ul>
-                  <div className="col-3 col-md-4">
-                    <h4 className="pt-3 text-170 text-600 text-purple-d1 letter-spacing">
-                      Plan
-                    </h4>
-                    <div className="text-secondary-d1 text-120">
-                      <span className="ml-n15 align-text-bottom">$</span>
-                      <span className="text-180">50</span> / M
-                    </div>
-                  </div>
-
-                  <div className="col-3 col-md-4 text-center">
-                    <h4 className="pt-3 text-170 text-600 text-purple-d1 letter-spacing">
-                      Client{" "}
-                    </h4>
-                    <h5> Ahmed A.</h5>
-                    <p>Cairo, Egypt</p>
-                  </div>
-                </div>
-              </div>
+                 
             </div>
           </div>
+         
         </div>
+
       </div>
     </div>
   );
--- src/pages/userPages/cleintProfile/profile.js
+++ src/pages/userPages/cleintProfile/profile.js
@@ -1,174 +1,136 @@
 import React from "react";
 import "./clientProfile.css";
 import { useState, useEffect } from "react";
-// import fileimg from '../../images/fileimg.jpg'
+import fileimg from '../../../images/fileimg.jpg'
 import { axiosInstace } from "../../../network/axiosConfig";
 import BTN from "../../../components/button/btn";
 
 export default function ClientProfile() {
-  const [Job, setJobs] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [UserDetails, setUserDetails] = useState([]);
+  const [Jobs, setJobs] = useState(null);
   useEffect(() => {
     axiosInstace
-      .get(`api/v1/users/621b9383236b5c2b1b76d6ec`)
-      .then((res) => {
-        setJobs(res.data.description);
+      .get("users/getMe")
+      .then(res => {
+        setUserDetails(res.data.data.data);
+        setJobs(res.data.data.data.jobs)
+        console.log("result", res.data.data.data);
+        console.log("jobs", res.data.data.data.jobs);
+        setIsLoading(false)
       })
-      .catch((err) => console.log(err));
-  }, [Job]);
+      .catch(err => console.log(err));
+  }, []);
 
   return (
     <div>
-      <div className="container">
-        <div className="container">
-          <div className="row">
-            <div className="col-sm p-10 m-5">
-              <h3> Your Dashboard </h3>
-              <h6> Doaa Eldesoki</h6>
+      <div class="container my-5">
+        <div class="container my-3">
+          <div class="row">
+            <div class="col-6 ">
+              <h3 className="mb-2"> Your Dashboard </h3>
+              <h6>{UserDetails.name}</h6>
             </div>
-            <div className="col-sm"></div>
-            <div className="d-flex justify-content-center align-items-center col-12 col-md-4">
-              <div id="BTN">
-                <BTN URL="/" text="Post a Job" type="defult" />
+            <div class="col-6">
+              <div className="my-3 my-md-0 text-end" >
+                <BTN URL="/ClientSetting" text="Settings" type="defult" />
               </div>
             </div>
           </div>
         </div>
 
         {/* secondsection */}
+        <div class="container">
 
-        <div
-          className="d-style btn btn-brc-tp border-2 bgc-white btn-outline-purple btn-h-outline-purple btn-a-outline-purple w-100 my-2 py-3 shadow-sm"
-          style={{ width: "50%" }}
-        >
-          <div className="row align-items-center">
-            <div className=" mb-0 col-12 col-md-8 text-dark-l1 text-90  my-4 my-md-0">
-              <div className="row">
-                <div className="col-sm-8 text-left m-2">
-                  {" "}
-                  <h3> Your Postings</h3>
+          <div
+            class=" w-100 my-2 py-3 shadow-sm"
+            style={{ width: "50%" }}>
+            <div class="row align-items-center">
+              <div class=" mb-0 col-12 col-md-8 text-dark-l1 text-90  my-4 my-md-0">
+                <div class="row">
+                  <div class="col-sm-8 text-left m-2">
+                    <h3> Your Postings</h3>
+                  </div>
                 </div>
               </div>
+              <br />
+              {/* <hr width='' /> */}
             </div>
-            <hr />
-            <div className="container joblist">
-              <div className="row">
-                <div className="col-sm-6 align-self-start">
-                  <h6 style={{ fontWeight: "bolder" }}>Landing Page</h6>
-                  <p>Public-Hourly</p>
-                  <p style={{ fontWeight: "lighter" }}>
-                    {" "}
-                    posted 18 Hours ago by You
-                  </p>
+          </div>
+        </div>
+        <br />
+
+        {
+          isLoading ?
+            "loading..."
+            : null
+        }
+
+        {
+          Jobs?.length == 0 ?
+            <div>
+              <div class="container">
+
+                <div class="d-flex justify-content-center">
+                  <img src={fileimg} />
                 </div>
-                <div className="col-sm-2">
-                  <p>
-                    {" "}
-                    <span style={{ color: "#fdc236", fontWeight: "bolder" }}>
-                      24
-                    </span>
-                  </p>
-                  <p> Proposals</p>
+                <div class="d-flex justify-content-center">
+                  <p> You don't have a  Posts Yet</p>
                 </div>
-                <div className="col-sm-2">
-                  <p>0</p>
-                  Messaged
-                </div>
-                <div className="col-sm-2">
-                  <p>0</p>
-                  <p> Hired</p>
-                </div>
-              </div>
-              <hr />
-              <div className="row">
-                <div className="col-sm-6 align-self-start">
-                  <h6 style={{ fontWeight: "bolder" }}>Landing Page</h6>
-                  <p>Public-Hourly</p>
-                  <p style={{ fontWeight: "lighter" }}>
-                    {" "}
-                    posted 18 Hours ago by You
-                  </p>
-                </div>
-                <div className="col-sm-2">
-                  <p>
-                    {" "}
-                    <span style={{ color: "#fdc236", fontWeight: "bolder" }}>
-                      24
-                    </span>
-                  </p>
-                  <p> Proposals</p>
-                </div>
-                <div className="col-sm-2">
-                  <p>0</p>
-                  Messaged
-                </div>
-                <div className="col-sm-2">
-                  <p>0</p>
-                  <p> Hired</p>
-                </div>
-              </div>
-              <hr />
-              <div className="d-flex justify-content-center">
-                <div className="d-flex justify-content-center align-items-center col-12 col-md-4">
-                  <div>
-                    <BTN URL="/" text="Post a Job" type="defult" />
+                <div class="d-flex justify-content-center">
+                  <div class="d-flex justify-content-center align-items-center col-12 col-md-4">
+                    {/* <div >
+                  <BTN URL="/" text="Post a Job" type="defult" />
+                </div> */}
                   </div>
                 </div>
               </div>
             </div>
 
-            {/* if no posts  */}
-
-            {/*
-            <div className="d-flex justify-content-center">
-              <img src={fileimg} />
+            : Jobs?.length > 0 &&
+            Jobs.map((job) => {
+              return (
+                <div class="container">
+                  <div class="row">
+                    <div class="col-sm-6 align-self-start">
+                      <h6 style={{ fontWeight: "bolder" }}> {job.description}</h6>
+                      <p>Public-Hourly</p>
+                      <p style={{ fontWeight: "lighter" }}>
+                        {" "}
+                        posted 18 Hours ago by You
+                      </p>
+                    </div>
+                    <div class="col-sm-2">
+                      <p>
+                        {" "}
+                        <span style={{ color: "#fdc236", fontWeight: "bolder" }}>
+                          {job.totalProposal}
+                        </span>
+                      </p>
+                      <p> Proposals</p>
+                    </div>
+                    <div class="col-sm-2">
+                      <p>{job.status}</p>
+                      <p> Status</p>
+                    </div>
+                  </div>
+                  <hr />
+                </div>
+              )
+            }
+            )}
+        <div class="d-flex justify-content-center">
+          <div class="d-flex justify-content-center align-items-center col-12 col-md-4">
+            <div className="my-3">
+              <BTN URL="/" text="Post a Job" type="defult" />
+              <br />
+              <br />
             </div>
-            <div className="d-flex justify-content-center">
-              <p> No Posts Yet</p>
-            </div>
-            <div className="d-flex justify-content-center">
-            <div className="d-flex justify-content-center align-items-center col-12 col-md-4">
-              <div >
-                <BTN URL="/" text="Post a Job" type="defult" />
-              </div>
-            </div>
-            </div> */}
+            <br />
           </div>
         </div>
       </div>
-    </div>
-  );
 
-  // return (
-  //   <>
-  //     <section classNameName="clientProfile">
-  //       <div classNameName="container">
-  //         <div classNameName="cleintProfileEditWidth">
-  //           <div classNameName="row profile-tile">
-  //             <div classNameName="col-sm-12 col-md-6">
-  //               <h4>Your Dashboard</h4>
-  //               <p classNameName="">Doaa Eldosoky</p>
-  //             </div>
-  //             <div classNameName="d-none d-md-block col-md-6 text-end">
-  //               <BTN URL="/" text="Post a Job" type="defult" />
-  //             </div>
-  //           </div>
-  //           <div classNameName="row ">
-  //             <div classNameName="clientJobs col-12 col-lg-8">
-  //               <div classNameName="row jobTitle">
-  //                 <h4>Your Postings</h4>
-  //               </div>
-  //               <div classNameName="row jobslist">
-  //                 <div classNameName="jobitem col-12 col-lg-8">
-  //                   <h5>Landing page js</h5>
-  //                   <p>Puplic</p>
-  //                   <p>Posted 2 days ago by you</p>
-  //                 </div>
-  //               </div>
-  //             </div>
-  //           </div>
-  //         </div>
-  //       </div>
-  //     </section>
-  //   </>
-  // );
-}
+    </div>
+  )
+} 
