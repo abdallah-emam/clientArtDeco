29a7c3b7b060a178d59c57f26ae0c79a4594c96a
Abdallah Hamdy <abdo.aa797@gmail.com>
2022-03-09 01:14:06 +0200

    add home styling

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -130,7 +130,7 @@ function Home() {
             <div className="row">
               <div className="col-12 for-client-go-to">
                 <div className="row">
-                  <div className="col-md-4 mt-3 pt-10">
+                  <div className="col-lg-4 d-flex align-items-stretch mt-3 pt-10">
                     <button className="card-link">
                       <h2>
                         Post a job <br /> and hire pro
@@ -141,7 +141,7 @@ function Home() {
                       </Link>
                     </button>
                   </div>
-                  <div className="col-md-4 mt-3  pt-10">
+                  <div className="col-lg-4 d-flex align-items-stretch mt-3 pt-10">
                     <button className="card-link">
                       <h2>
                         Browse <br /> and buy project
@@ -149,7 +149,7 @@ function Home() {
                       <Link to="/"> Project list &#x2192; </Link>
                     </button>
                   </div>
-                  <div className="col-md-4  mt-3  pt-10">
+                  <div className="col-lg-4 d-flex align-items-stretch mt-3 pt-10">
                     <button className="card-link">
                       <h2>
                         Countact us <br /> to help you in any problem
